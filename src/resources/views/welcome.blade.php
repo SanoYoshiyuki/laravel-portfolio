7c773dff9f778fa4542085af7d9b76e89d42ad65
kuniyuki hirota <kuniyukihirota@sano-yoshiyuki.local>
2020-05-12 22:20:14 +0900

    topvideo差し込み

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -11,16 +11,27 @@
         <link href="/css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="content">
-            <nav id="headerNav">
+        <div class="content flex">
+            <nav id="headerNav" class="justify-content fiexd flex w-100">
                 <div class="flex w-100">
                     <a class="flex flex-center">
-                        <img src="{{ asset('/images/logo.svg') }}" />
+                        <img src="{{ asset('/images/logo_stellargo-white.svg') }}" />
                     </a>
                 </div>
             </nav>
-            <div class="test-title">
-                <p>test</p>
+            <div id="upparContent">
+                <div>
+                    <video autoplay muted loop id="topVideo" class="fiexd">
+                        <source src="{{ asset('/video/card-flipping-desktop.mp4') }}" type="video/mp4">
+                    </video>
+                    <div class="relative center">
+                        <div class="flex flex-column flex-center main-width hero">
+                            <img src="{{ asset('/images/logo_stellargo-white.svg') }}">
+                            <h1>Global Financial Services</h1>
+                            <p>The Stellargo ecosystem provides users with a basic function that allows them to have their own bank accounts and cryptocurrency wallets within their individual app. It also stores legal currencies and cryptocurrencies and provides the ability to use legal currencies and cryptocurrencies in different payments in one app. For example,</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
